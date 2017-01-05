7eb8ee4794084e277b7f21b7cf63e479e310ca6e
Monika <monika.liutk@gmail.com>
2017-01-05 15:15:06 +0200

    lighter loading

--- scroller.js
+++ scroller.js
@@ -2,19 +2,21 @@ var counter = 1;
 var per_page = 20;
 var loadAllowed = true;
 
-var batch;
+var shots = [];
 
 var loadMore =  function(evt) {
-	if ((window.innerHeight + window.scrollY) > document.body.scrollHeight - 400 && loadAllowed) {
+	if ((window.innerHeight + window.scrollY) > document.body.scrollHeight - 200 && loadAllowed) {
 		loadAllowed = false;
-		console.log(counter);
-		requestShots();
+		addShots();
+		if ((window.innerHeight + window.scrollY) > document.body.scrollHeight - 200) {
+			requestShots();
+		}
+		loadAllowed = true;
 	}
 };
 
 requestShots();
 document.addEventListener("scroll", loadMore);
-
 
 function requestShots() {	
 	var xhr = new XMLHttpRequest();
@@ -24,7 +26,8 @@ function requestShots() {
 
 	xhr.onload = function (e) {
 		if (xhr.readyState === 4 && xhr.status === 200) {
-			addShots(xhr.responseText)
+			shots = JSON.parse(xhr.responseText);
+			addShots();
 		}
 	};
 
@@ -36,25 +39,31 @@ function requestShots() {
 
 }
 
-function addShots (responseText) {
-	var obj = JSON.parse(responseText);
+function addShots () {
 	var i = 0;
-
-	while (i < Object.keys(obj).length) {
-		if (obj[i].animated == true || window.screen.width >= 1600) {
-			var image = obj[i].images.hidpi;
-		} else {
-			var image = obj[i].images.normal;
-		}
-		document.getElementById("scroll").innerHTML = document.getElementById("scroll").innerHTML 
-			+ '<div class="image">'
-				+ '<img class="blur" src="' + image + '">' 
-				+ '<h2>' 
-					+ '<div class="text shot_title">'+ obj[i].title +'</div>' 
-					+ '<div class="text shot_author"><div class="line"></div><div>' + obj[i].user.name +' </div></div>' 
-				+ '</h2>' 
-			+ '</div>';
+	while (i < Object.keys(shots).length && (window.innerHeight + window.scrollY) > document.body.scrollHeight - 200) {
+		addShot(shots[i]);
+		console.log((window.innerHeight + window.scrollY) > document.body.scrollHeight - 200);
 		i++;
 	}
-	loadAllowed = true;
+	console.log(i);
+	shots.splice(0, i);	
 }
+
+function addShot(shot) {
+	if ((shot.animated == true || window.screen.width >= 1600) && shot.images.hidpi != null) {
+		var image = shot.images.hidpi;
+	} else {
+		var image = shot.images.normal;
+	}
+	document.getElementById("scroll").innerHTML = document.getElementById("scroll").innerHTML 
+		+ '<div class="image">'
+			+ '<img class="blur" src="' + image + '">' 
+			+ '<h2>' 
+				+ '<div class="text shot_title">'+ shot.title +'</div>' 
+				+ '<div class="text shot_author"><div class="line"></div><div>' + shot.user.name +'</div></div>' 
+			+ '</h2>' 
+		+ '</div>';
+}
+
+
